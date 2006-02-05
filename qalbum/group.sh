ebc9e4628c17d455c5cded9e3dc0e7ee9c235211
bothner <bothner>
2006-02-05 07:44:13 +0000

    Don't use jhead image - we now extract EXIF data in ImageInfo.java, using Drew Noakes's metadata library.

--- qalbum/group.sh
+++ qalbum/group.sh
@@ -12,18 +12,9 @@ for file in picture.js group.js help.html; do
   test $scriptdir/$file -nt $libdir/$file && cp $scriptdir/$file $libdir/$file
 done
   
-for id in `sed -n -e 's/<picture id="\(.*\)">/\1/p' <index.xml`
-do
-  infofile="$id"-info.txt
-  filename=`sed -n -e '/<picture id="'$id'">/,/<.picture>/s/<[a-z]*-*image[^>]*>\(.*[0-9]\)[tpr]*[.]jpg<[/].*image>/\1.jpg/p' <index.xml | uniq`
-  echo '<pre>' >$infofile
-  $JHEAD $filename >>$infofile
-  echo '</pre>' >>$infofile
-done
-
 #$KAWA -xquery -f $scriptdir/pictures.xql
 #(cd $scriptdir; $KAWA -xquery --main -C pictures.xql)
-$JAVA -cp $scriptdir:${KAWAJAR} pictures libdir=$libdir
+$JAVA -cp $scriptdir/..:${KAWAJAR}:${ExifExtractor_JAR} qalbum.pictures libdir=$libdir
 
 #export KAWAJAR=/tmp/kawa-1.6.99.jar
 #CLASSPATH=$PICLIBDIR/exif/exifExtractor.jar $JAVA kawa.repl -xquery --debug-print-expr -f $scriptdir/pictures.xql
@@ -32,5 +23,3 @@ $JAVA -cp $scriptdir:${KAWAJAR} pictures libdir=$libdir
 #echo CLASSPATH='"'$CLASSPATH'"'
 #$JAVA kawa.repl -xquery --main -C $scriptdir/pictures.xql
 #$JAVA pictures
-
-rm -f *-info.txt
