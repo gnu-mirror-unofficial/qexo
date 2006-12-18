70eb79e6a64370b561aea131ede0ea0c79b31ff8
bothner <bothner>
2006-12-18 01:57:21 +0000

    Remove no-longer used files.

--- qalbum/make-xml.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# Usage make-xml.sh title file.jpg ...
-scriptdir=$(dirname $(which $0))
-. $scriptdir/setup.sh
-exec $JAVA -cp $scriptdir:${KAWAJAR}:${ExifExtractor_JAR}:$scriptdir/.. qalbum.create "$@"
-exit -1
